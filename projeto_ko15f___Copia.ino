1844a80dc491548a1d74982071ec56190c7f4a56
Geovane-p <168130293+Geovane-p@users.noreply.github.com>
2024-10-28 10:21:47 -0300

    atualização com LDR

--- projeto_ko15f___Copia.ino
+++ projeto_ko15f___Copia.ino
@@ -1,33 +1,34 @@
 int releExaustor = 13;
 int releGas = 12;
 int torn = 11;
-int chama = 10;
 int centelha = 9;
+#define ldr A0
+int vldr = 0;
 
 void setup() {
   pinMode(releExaustor, OUTPUT);
   pinMode(releGas, OUTPUT);
   pinMode(torn, INPUT_PULLUP);
-  pinMode(chama, INPUT_PULLUP);
   pinMode(centelha, OUTPUT);
+  pinMode(ldr, INPUT);
 
   digitalWrite(releExaustor, HIGH);
   digitalWrite(releGas, HIGH);
 }
 
 void loop() {
-  if (digitalRead(torn) == HIGH && digitalRead(chama) == LOW) {
+  if (digitalRead(torn) == HIGH && vldr == 0) {
     digitalWrite(releExaustor, LOW);
     delay(2000);
 
-    if (digitalRead(torn) == HIGH && digitalRead(chama) == LOW) {
+    if (digitalRead(torn) == HIGH && vldr == 0) {
       digitalWrite(releExaustor, LOW);
       digitalWrite(centelha, LOW);
       delay(1500);
       digitalWrite(releGas, LOW);
       delay(1700);
 
-      if (digitalRead(torn) == HIGH && digitalRead(chama) == HIGH) {
+      if (digitalRead(torn) == HIGH && vldr > 4) {
         digitalWrite(releExaustor, LOW);
         digitalWrite(releGas, LOW);
         digitalWrite(centelha, HIGH);
