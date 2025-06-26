e747857fa27d27fee6eebab6e31c7b37a7153bfb
Geovane-p <168130293+Geovane-p@users.noreply.github.com>
2025-06-26 10:10:17 -0300

    Update projeto_ko15f___Copia.ino
    A base, mas agora com o ldr implementado

--- projeto_ko15f___Copia.ino
+++ projeto_ko15f___Copia.ino
@@ -1,47 +1,65 @@
 int releExaustor = 13;
 int releGas = 12;
 int torn = 11;
-int chama = 10;
+int ledchama = 10;
 int centelha = 9;
+#define ldr A0
+
+bool sequenciaIniciada = false;
+int vldr = 0;  // Variável para armazenar valor do LDR
 
 void setup() {
   pinMode(releExaustor, OUTPUT);
   pinMode(releGas, OUTPUT);
   pinMode(torn, INPUT_PULLUP);
-  pinMode(chama, INPUT_PULLUP);
+  pinMode(ledchama, OUTPUT);
   pinMode(centelha, OUTPUT);
+  pinMode(ldr, INPUT);
 
-  digitalWrite(releExaustor, HIGH);
-  digitalWrite(releGas, HIGH);
+  Serial.begin(9600);
+
+  apagarTudo();
 }
 
 void loop() {
-  if (digitalRead(torn) == HIGH && digitalRead(chama) == LOW) {
-    digitalWrite(releExaustor, LOW);
+  vldr = analogRead(ldr);         // Lê valor do LDR
+  Serial.println(vldr);           // Exibe no monitor serial
+
+  if (digitalRead(torn) == LOW && !sequenciaIniciada) {
+    sequenciaIniciada = true;
+
+    digitalWrite(releExaustor, HIGH);
     delay(2000);
 
-    if (digitalRead(torn) == HIGH && digitalRead(chama) == LOW) {
-      digitalWrite(releExaustor, LOW);
-      digitalWrite(centelha, LOW);
+    if (digitalRead(torn) == LOW) {
+      digitalWrite(centelha, HIGH);
       delay(1500);
-      digitalWrite(releGas, LOW);
+
+      digitalWrite(releGas, HIGH);
       delay(1700);
 
-      if (digitalRead(torn) == HIGH && digitalRead(chama) == HIGH) {
-        digitalWrite(releExaustor, LOW);
-        digitalWrite(releGas, LOW);
-        digitalWrite(centelha, HIGH);
-
+      if (digitalRead(torn) == LOW && vldr > 100) {
+        digitalWrite(ledchama, HIGH);
+        delay(1700);
+        digitalWrite(centelha, LOW);
       } else {
-        digitalWrite(releExaustor, HIGH);
-        digitalWrite(releGas, HIGH);
+        apagarTudo();
       }
     } else {
-      digitalWrite(releExaustor, HIGH);
-      digitalWrite(releGas, HIGH);
+      apagarTudo();
     }
-  } else {
-    digitalWrite(releExaustor, HIGH);
-    digitalWrite(releGas, HIGH);
+
+  } else if (digitalRead(torn) == HIGH) {
+    sequenciaIniciada = false;
+    apagarTudo();
   }
+
+  delay(100);  // Apenas para não sobrecarregar a leitura serial
+}
+
+void apagarTudo() {
+  digitalWrite(releExaustor, LOW);
+  digitalWrite(releGas, LOW);
+  digitalWrite(centelha, LOW);
+  digitalWrite(ledchama, LOW);
 }
